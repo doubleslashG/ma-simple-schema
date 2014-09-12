504f7068b4f4a3463b575c4d946a7eef54535086
fissore <giancarlo.fissore@gmail.com>
2014-09-12 17:10:23 +0200

    Updated package.js

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
-  summary: " \* Fill me in! *\ ",
-  version: "1.0.0",
-  git: " \* Fill me in! *\ "
+  summary: "aldeed:simple-schema extended",
+  version: "0.0.0",
+  git: "https://github.com/doubleslashG/ma-simple-schema.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.1.1');
   api.addFiles('ma:simple-schema.js');
+  api.use('aldeed:simple-schema');
 });
 
 Package.onTest(function(api) {
